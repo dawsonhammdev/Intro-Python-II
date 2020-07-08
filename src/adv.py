ba5abf81bf84f09947b1f752013dba01fcc73869
dawsonhammdev <hammsdesigns@gmail.com>
2020-07-08 17:51:20 -0500

    Player is able to move around the map

--- src/adv.py
+++ src/adv.py
@@ -4,7 +4,8 @@ from player import Player
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance","""North of you, the cave mount beckons"""),
+    'outside':  Room("Outside Cave Entrance",
+                     "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -22,7 +23,7 @@ earlier adventurers. The only exit is to the south."""),
 }
 
 
-# Link rooms togetherclear
+# Link rooms together
 
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
@@ -37,16 +38,26 @@ room['treasure'].s_to = room['narrow']
 # Main
 #
 
+# tries to move the player in the specified direction 
 def try_direction(player, direction):
+    # check the player's current location and see if there is 
+    # a room in the specified direction 
+    # if there is, move them there to that room 
+    # otherwise, print a message saying "we can't go there" and 
+    # not move the player 
     attribute = direction + '_to'
 
+    # Python has a handy method called `hasattr`
+    # which allows us to check if a class has an attribute 
     if hasattr(player.location, attribute):
-        player.loction = getattr(player.location, attribute)
+        # this is valid direction 
+        # use getattr to fetch the value associated with the attribute 
+        # update our player's location with the fetched room 
+        player.location = getattr(player.location, attribute)
     else:
-        print("There is nothing in that direction")
+        print("There's nothing in that direction!")
 
 # Make a new player object that is currently in the 'outside' room.
-
 player = Player(room['outside'])
 
 # Write a loop that:
@@ -63,13 +74,12 @@ while True:
     # If the user enters "q", quit the game.
     if first_char == 'q':
         break
-
-    # If the user enters "q", quit the game.
     #
     # If the user enters a cardinal direction, attempt to move to the room there.
     # Print an error message if the movement isn't allowed.
-    # User can enter N, S, E, W
-    # Strip off everything but the first character
+    # User can enter 'north', 'south', 'east', 'west', or just allow them to 
+    # enter 'n', 's', 'e', 'w' in order to move 
+    # strip off everything but the first char 
 
     if first_char == 'n':
         # move to the north
@@ -83,6 +93,3 @@ while True:
     elif first_char == 'w':
         # move to the west 
         try_direction(player, first_char)
-
-    
-   
